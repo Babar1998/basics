d2e84093de38e2869d45ef3ccbc5db20fb4b24a9
Babar Arif <babararifmasood98@gmail.com>
2024-02-17 21:11:47 +0500

    Learning

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
